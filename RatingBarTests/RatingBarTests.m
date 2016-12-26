5137713253b4b8ddc9feac38b759fc7d96bff765
chw <hanwen@HanWendeiMac.local>
2016-12-26 22:04:27 +0800

    Initial Commit

---
+++ RatingBarTests/RatingBarTests.m
@@ -0,0 +1,39 @@
+//
+//  RatingBarTests.m
+//  RatingBarTests
+//
+//  Created by HanWen on 2016/12/26.
+//  Copyright © 2016年 HanWen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RatingBarTests : XCTestCase
+
+@end
+
+@implementation RatingBarTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
